6f8328ce5d2a32407efc6173aa3f9518c3bea6ca
Jordan Blissett <jblissett7@gmail.com>
2023-06-03 16:46:38 -0500

    Initial Commit

---
+++ CharliesSpades/CharliesSpadesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CharliesSpadesApp.swift
+//  CharliesSpades
+//
+//  Created by Jordan Blissett on 6/3/23.
+//
+
+import SwiftUI
+
+@main
+struct CharliesSpadesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
