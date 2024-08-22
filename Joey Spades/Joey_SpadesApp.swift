abf75bb8c57b26e91c0527cceb8ebeb21eeb2b4b
Jordan Blissett <jblissett7@gmail.com>
2024-08-22 13:15:56 -0500

    Initial Commit

---
+++ Joey Spades/Joey_SpadesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Joey_SpadesApp.swift
+//  Joey Spades
+//
+//  Created by Jordan Blissett on 8/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Joey_SpadesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
